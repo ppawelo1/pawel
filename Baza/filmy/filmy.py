8d41e436b2bfdc4bdff70bece4940b7865321fe3
ppawelo1 <pawel.petelewicz@op.pl>
2018-06-08 13:31:28 +0200

    terminal

--- Baza/filmy/filmy.py
+++ Baza/filmy/filmy.py
@@ -7,26 +7,29 @@ import csv
 
 def dane_z_pliku(nazwa_pliku):
     dane = []  # pusta lista na dane
-    with open(nazwa_pliku, newline='', encoding='utf-8') as plik:
+    with open(nazwa_pliku, 'r', newline='', encoding='utf-8') as plik:
         tresc = csv.reader(plik, delimiter='\t')
         for rekord in tresc:
-            rekord = [x.stript() for x in rekord]  # usuwanie zbednych spacji
-            dane.append(rekord)
+            rekord = [x.strip() for x in rekord]  # oczyszczamy dane
+            dane.append(rekord)  # dodawanie rekordów do listy
     return dane
 
 
 def main(args):
-    con = sqlite3.connect('filmy.db')
+    con = sqlite3.connect('filmy.db')  # połączenie z bazą
     cur = con.cursor()  # utworzenie kursora
 
+    # utworzenie tabeli w bazie
     with open('filmy.sql', 'r') as plik:
         cur.executescript(plik.read())
 
-        filmy = dane_z_pliku('filmy.txt')
-        filmy.pop(0)  # usunięcie pierwszego elementu listy
-        cur.executemany('INSERT INTO filmy VALEUS(?, ?, ?, ?, ?)', filmy)
+    # dodawanie danych do bazy
+    filmy = dane_z_pliku('filmy.txt')
+    filmy.pop(0)  # usuń pierwszy rekord z listy
+    cur.executemany('INSERT INTO filmy VALUES(?, ?, ?, ?, ?)', filmy)
 
-    con.commit()   # zatwierdzenie wszystkich operacji w bazie
+    con.commit()  # zatwierdzenie zmian w bazie
+    con.close()  # zamknięcie połączenia z bazą
     return 0
 
 
