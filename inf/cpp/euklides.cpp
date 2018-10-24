9f68512fdbfb5959b92339204ca3f1b4a29f7cb5
ppawelo1 <pawel.petelewicz@op.pl>
2018-10-24 10:34:16 +0200

    eukliades i nowe rzeczy

---
+++ inf/cpp/euklides.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+
+int nwd_klasyczny(int a, int b) {
+    int licznik = 0;
+    while(a > b){
+        if(a > b)
+            a = a - b;
+        else
+            b = b - a;
+            licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return a;
+}
+
+int nwd_optymalny(int a, int b) {
+    int licznik = 0;
+    while(a > 0) {
+        a = a % b;
+        b = b - a;
+        licznik++;
+        }
+        cout << "Powtórzeń: " << licznik << endl;
+        return b;
+}
+
+int main( int argc, char **argv)
+{
+    
+    int a, b;
+    cout << "Podaj dwie liczby: ";
+    cin >> a >> b;
+    int nwd = nwd_klasyczny(a, b);
+    cout << "NWD(" << a << b << ") =" << nwd << endl;
+    nwd = nwd_optymalny(a, b);
+    cout << "NWD(" << a << b << ") =" << nwd << endl;
+	
+	return 0;
+}
---
+++ inf/cpp/minmax/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+void minmax1() {
+    char dalej = 't';
+    while(dalej = 't') {
+        cout << "Podaj liczbę: ";
+    }
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
