764cb13f6e4f8eaba37a737a504963380e2c1ba7
ppawelo1 <pawel.petelewicz@op.pl>
2018-10-25 08:45:12 +0200

    liczby randomowe

--- inf/cpp/minmax/main.cpp
+++ inf/cpp/minmax/main.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
+
+
 void minmax1() {
     char dalej = 't';
-    while(dalej = 't') {
+    int liczba = 0;
+    int min, max;
+    cout << "Podaj liczbę: ";
+    cin >> liczba;
+    min = max = liczba;
+    while(dalej == 't') {
         cout << "Podaj liczbę: ";
+        cin >> liczba;
+        if (liczba < min)
+            min = liczba;
+        if (liczba > max)
+            max = liczba;
+        cout << "Nastepna (t/n)? ";
+        cin >> dalej;
+    }
+    cout << "Najmniejsza: " << min << endl;
+    cout << "Najwieksza: " << max << endl;
+}
+
+void wypelnij(int tab[], int roz) {
+    cout << "Podaj " << roz << " liczb: " << endl;
+    for(int i=0; i < roz; i++) {
+        cin >> tab[i];
+    }
+}
+
+void wypelnij_los(int tab[], int roz) {
+    srand(time(NULL)); //inicjacja generatora liczb pseudolosowych
+    for(int i=0; i < roz; i++) {
+        tab[i] = rand() % 101;
+    }
+}
+
+
+void drukuj(int tab[], int roz) {
+    for(int i=0; i < roz; i++) {
+        cout << tab[i] << " ";
     }
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int rozmiar = 60;
+    int tab[rozmiar];
+    wypelnij_los(tab, rozmiar);
+    drukuj(tab, rozmiar);
+    //minmax1();
     return 0;
 }
