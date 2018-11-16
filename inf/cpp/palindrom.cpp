cd8f88af477734e584c47e37ce141de8076a4e98
ppawelo1 <pawel.petelewicz@op.pl>
2018-11-16 14:19:15 +0100

    eeee

---
+++ inf/cpp/palindrom.cpp
@@ -0,0 +1,32 @@
+/*
+ * palindrom.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+ */
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+bool palindrom(char w[], int r) {
+    bool zliczona = false;
+    for(int i = 0; i < (ilosc / 2); i++) {
+        if(wyraz[i] == wyraz[ilosc - (i+1)])
+        }
+    return pal;
+}
+
+int main(int argc, char **argv)
+{
+	int roz = 20;
+    char wyraz[roz];
+    cout << "Podaj wyraz: ";
+    cin.getline (wyraz, roz);
+    cout << cin.gcount() << endl;
+    cout << strlen(wyraz) << endl;
+	return 0;
+}
+
