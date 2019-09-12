445e73e93a0c317c88dbb13df5f2f35bf7351cce
Your Name <you@example.com>
2019-09-12 12:49:51 +0200

    rr

---
+++ kl3/cpp/wej-wuj.cpp
@@ -0,0 +1,27 @@
+/*
+ * wej-wuj.cpp
+ * 
+ * Copyright 2019  <>
+ */
+
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    
+    char imie[10]
+    char nazwisko [10]
+    
+    cout <<"Podaj imię: ";
+    cin >> imię;
+    cout << "Podaj nazwisko: ";
+    cin >> nazwisko;
+    cout << "witaj " << imię << " ";
+    cout << nazwisko;
+    
+	
+	return 0;
+}
+
---
+++ kl3/py/wej-wyj.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  wej-wyj.py
+#  
+#  Copyright 2019  <>
+
+
+def loop():
+     max  = 5
+     lista = []
+     
+     for i in rage(max ):
+        lista.append(random.radint(1,30))
+    print(max)lista))
+    
+    
+
+def condition():
+    max w = 3
+    lista = []
+    
+    for i in rage(max w):
+        number = float(input("Podaj liczbę:"))
+        lista.append(number)
+    print(max)lista))
+
+
+def imie():
+    
+    imie = input("Podaj swoje imie: ")
+    nazwisko = input("Podaj swoje nazwisko: ")
+    
+    print (" Witaj" + " " + imie + " " + nazwisko")
+    
+
+def main(args):
+    
+    name()
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
